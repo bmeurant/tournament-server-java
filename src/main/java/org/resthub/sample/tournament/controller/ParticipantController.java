e15f949ba5c60cab4443e9b74a0c3bd00779994c
Baptiste Meurant <baptiste.meurant@gmail.com>
2012-08-21 21:57:59 +0200

    custom pagination API : 0-indexed to 1-indexed

--- src/main/java/org/resthub/sample/tournament/controller/ParticipantController.java
+++ src/main/java/org/resthub/sample/tournament/controller/ParticipantController.java
@@ -1,13 +1,17 @@
 package org.resthub.sample.tournament.controller;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import org.resthub.sample.tournament.model.Participant;
 import org.resthub.sample.tournament.repository.ParticipantRepository;
 import org.resthub.web.controller.RepositoryBasedRestController;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/api/participant")
@@ -24,4 +28,15 @@ public class ParticipantController extends RepositoryBasedRestController<Partici
     public Long getIdFromResource(Participant resource) {
         return resource.getId();
     }
+
+    /**
+     * resthub uses 0-indexed pages, but I want my service interface to use 1-indexed pages
+     * @see org.resthub.web.controller.RestController#findPaginated
+     */
+    @Override
+    public Page<Participant> findPaginated(@RequestParam(value = "page", required = true) Integer pageId,
+                                           @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+        return super.findAll( pageId - 1, size);
+    }
+
 }
