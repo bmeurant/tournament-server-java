56b0f09b583b13019a07c7dc588ccdaa82360013
Baptiste Meurant <baptiste.meurant@gmail.com>
2012-08-22 09:59:13 +0200

    implement post and get Photo

--- src/main/java/org/resthub/sample/tournament/controller/ParticipantController.java
+++ src/main/java/org/resthub/sample/tournament/controller/ParticipantController.java
@@ -1,16 +1,30 @@
 package org.resthub.sample.tournament.controller;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.resthub.sample.tournament.model.Participant;
 import org.resthub.sample.tournament.repository.ParticipantRepository;
 import org.resthub.web.controller.RepositoryBasedRestController;
+import org.resthub.web.exception.BadRequestException;
+import org.resthub.web.exception.InternalServerErrorException;
+import org.resthub.web.exception.NotFoundException;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.util.Assert;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.List;
 
 @Controller
@@ -31,12 +45,85 @@ public class ParticipantController extends RepositoryBasedRestController<Partici
 
     /**
      * resthub uses 0-indexed pages, but I want my service interface to use 1-indexed pages
+     *
      * @see org.resthub.web.controller.RestController#findPaginated
      */
     @Override
     public Page<Participant> findPaginated(@RequestParam(value = "page", required = true) Integer pageId,
                                            @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
-        return super.findAll( pageId - 1, size);
+        return super.findAll(pageId - 1, size);
+    }
+
+    @RequestMapping(value = "{id}/photo", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void storePhoto(@PathVariable("id") Long participantId, @RequestParam MultipartFile file) {
+
+        Assert.notNull(participantId, "id cannot be null");
+
+        Participant participant = this.repository.findOne(participantId);
+        if (participant == null) {
+            throw new NotFoundException();
+        }
+
+        // remove photo if already exists
+        File oldFile = participant.getPictureFile();
+        if (null != oldFile) {
+            oldFile.delete();
+        }
+
+        String type = file.getContentType().split("/")[1];
+        String fileNameToCreate = "D:/Dev/" + "participant/" + participantId + "." + type;
+
+        File newFile = new File(fileNameToCreate);
+        try {
+            FileUtils.writeByteArrayToFile(newFile, file.getBytes());
+        } catch (IOException e) {
+            logger.error("Cannot read upload file", e);
+            throw new InternalServerErrorException("Cannot read upload file", e);
+        }
+    }
+
+    @RequestMapping(value = "{id}/photo", method = RequestMethod.GET)
+    public void getPhoto(@PathVariable("id") Long participantId, HttpServletResponse resp) {
+
+        Assert.notNull(participantId, "id cannot be null");
+
+        Participant participant = this.repository.findOne(participantId);
+        if (participant == null) {
+            throw new NotFoundException();
+        }
+
+        File file = participant.getPictureFile();
+        if (file == null) {
+            throw new NotFoundException();
+        }
+
+        resp.reset();
+        byte[] content;
+        InputStream fileInputStream;
+
+        try {
+            fileInputStream = new FileInputStream(file);
+            content = IOUtils.toByteArray(fileInputStream);
+        } catch (IOException e) {
+            logger.error("Cannot read file", e);
+            throw new InternalServerErrorException("Cannot read file", e);
+        }
+
+        String fileName = file.getName();
+        String type = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
+        resp.setContentType("image/" + type);
+        resp.setHeader("Accept-Ranges", "bytes");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        resp.setContentLength(content.length);
+
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+            FileCopyUtils.copy(in, resp.getOutputStream());
+            resp.flushBuffer();
+        } catch (IOException e) {
+            throw new InternalServerErrorException("cannot read image file", e);
+        }
     }
 
 }
--- src/main/java/org/resthub/sample/tournament/model/Participant.java
+++ src/main/java/org/resthub/sample/tournament/model/Participant.java
@@ -1,11 +1,15 @@
 package org.resthub.sample.tournament.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.Email;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlTransient;
+import java.io.File;
 
 @Entity
 public class Participant {
@@ -14,6 +18,7 @@ public class Participant {
     private String firstname;
     private String lastname;
     private String email;
+    private Boolean hasPicture;
 
     public Participant() {
         super();
@@ -65,5 +70,47 @@ public class Participant {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Transient
+    public String getPictureUrl() {
+        if (this.hasPicture == null) {
+            if (this.getPictureFile() != null) {
+                this.hasPicture = Boolean.TRUE;
+            }
+        }
+
+        return (Boolean.TRUE == this.hasPicture) ? "/participant/" + this.id + "/photo" : null;
+    }
+
+    @Transient
+    public String getPictMin() {
+        if (this.hasPicture == null) {
+            if (this.getPictureFile() != null) {
+                this.hasPicture = Boolean.TRUE;
+            }
+        }
+
+        return (Boolean.TRUE == this.hasPicture) ? "/participant/" + this.id + "/min-photo" : null;
+    }
+
+    @Transient
+    @XmlTransient
+    @JsonIgnore
+    public File getPictureFile() {
+        File file = null;
+
+        String[] formats = {"gif", "GIF", "jpg", "jpeg", "JPG", "JPEG", "png", "PNG"};
+        String fileName = "D:/Dev/" + "participant/" + this.id;
+
+        for (String format : formats) {
+            File tmpFile = new File(fileName + "." + format);
+            if (tmpFile.exists()) {
+                file = tmpFile;
+                break;
+            }
+        }
+
+        return file;
     }
 }
